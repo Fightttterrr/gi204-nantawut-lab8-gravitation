cd759301375eef3c01962c4ee6aeecf977687389
Fighttttz <nantawut.buat@bumail.net>
2025-03-11 00:23:05 +0700

    Add Script และ จำลองโลกดึงดูดดาวดวงอื่น

---
+++ Assets/Scripts/Gravity.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour
+{
+    Rigidbody rb;
+    const float G = 0.0066667f;
+    public static List<Gravity> planetLists;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        if (planetLists == null)
+        {
+            planetLists = new List<Gravity>();
+        }
+        
+        planetLists.Add(this);
+    }
+
+    private void FixedUpdate()
+    {
+        foreach (var planet in planetLists)
+        {
+            if (planet != this)
+                Attract(planet);
+        }
+    }
+
+
+    void Attract(Gravity other)
+    {
+        Rigidbody otherRb = other.rb;
+        
+        Vector3 direction = rb.position - otherRb.position;
+        float distance = direction.magnitude;
+        
+        float forceMagnitude = G * ((rb.mass * otherRb.mass)/Mathf.Pow(distance, 2));
+        Vector3 finalForce = forceMagnitude * direction.normalized;
+        
+        otherRb.AddForce(finalForce);
+    }
+}
